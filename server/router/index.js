eda9cec59165144c0cdee2e4fa47dc7f1d1eabb5
LuffyKing <aderinwale17@gmail.com>
2018-05-18 22:42:25 +0100

    [Feature #157581106] Added validation middleware to createARequest

--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import RequestsController from '../controllers/Requests';
 import { getARequestChecker } from '../validation/getARequestValidator';
+import { createARequestChecker } from '../validation/createARequestValidator';
 
 const router = express.Router();
 
 router.get('/users/requests', RequestsController.getAllRequests);
 router.get('/users/requests/:requestid', getARequestChecker, RequestsController.getARequest);
-router.post('/users/requests/', RequestsController.createARequest);
+router.post('/users/requests/', createARequestChecker, RequestsController.createARequest);
 router.put('/users/requests/:requestid', getARequestChecker, RequestsController.updateARequest);
 export default router;
