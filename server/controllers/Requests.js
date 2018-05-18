8477c8318ca728821282d7fd5a49e55dfa73e4b9
LuffyKing <aderinwale17@gmail.com>
2018-05-18 10:51:18 +0100

    [Feature #157581106] Setup route handler for modifyARequest operation

--- server/controllers/Requests.js
+++ server/controllers/Requests.js
@@ -63,6 +63,47 @@ request is made
       request: newRequest,
       message: 'Success - repair/maintenance request created.'
     });
+  },
+  /**
+* It gets a requests on the application
+* @param {Object} req - request object containing params and body
+* @param {Object} res - response object that conveys the result of the request
+* @returns {Object} - response object that has a status code of 200 and
+* a repair or maintenance request that has been updated or 404 if
+*the id provided in the req params id does not match an existing request
+*/
+  updateARequest: (req, res) => {
+    const {
+      title,
+      description,
+      location,
+      type
+    } = req.body;
+    const {
+      requestid
+    } = req.params;
+    const result = requests.filter(request => request.id === Number(requestid));
+
+    if (result.length > 0) {
+      const updatedRequest = {
+        id: requestid,
+        userid: result.userid,
+        status: result.status,
+        dateSubmitted: result.dateSubmitted,
+        title: title || result.title,
+        description: description || result.description,
+        location: location || result.location,
+        type: type || result.type,
+      };
+      requests.push(updatedRequest);
+      return res.status(200).send({
+        message: 'Success - repair/maintenance request updated.',
+        updatedRequest
+      });
+    }
+    return res.status(404).send({
+      message: 'Maintenance/Repair with the specified id was not found'
+    });
   }
 
 };
