a749904c34df80fb186173faba1750e6be4106d9
LuffyKing <aderinwale17@gmail.com>
2018-05-18 05:34:34 +0100

    [Feature #157580836] Setup route handler in RequestsController

--- server/controllers/Requests.js
+++ server/controllers/Requests.js
@@ -1,19 +1,36 @@
 import { requests } from '../dummy-data/database';
 /**
- * An that handles the requests api operation
+ * An  object that handles the requests api operation
  */
 const Requests = {
   /**
 * It gets all the requests on the application
 * @param {Object} req - request object containing params and body
 * @param {Object} res - response object that conveys the result of the request
-* @returns {Object} - response object that has a status code of either 201 and
-* the new business or 400 depending on whether completness of the information
-* posted or the uniqness of the email and telephoneNumber
+* @returns {Object} - response object that has a status code of 200 as long as a
+request is made
 */
   getAllRequests: (req, res) => res.status(200).send({
     message: 'Success - All repair/maintenance requests retrieved.',
     requests
   }),
+  /**
+* It gets a requests on the application
+* @param {Object} req - request object containing params and body
+* @param {Object} res - response object that conveys the result of the request
+* @returns {Object} - response object that has a status code of either 200 and
+* a repair or maintenance request or 404 if the id provided in the req params id
+* does not match an existing request
+*/
+  getARequest: (req, res) => {
+    const { requestid } = req.params;
+    const result = requests.filter(request => request.id === Number(requestid));
+    return result.length > 0 ? res.status(200).send({
+      message: 'Success - repair/maintenance request retrieved.',
+      request: result[0]
+    }) : res.status(404).send({
+      message: 'Maintenance/Repair with the specified id was not found'
+    });
+  }
 };
 export default Requests;
