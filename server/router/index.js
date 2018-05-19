9d41f97a0998589e7f8479ad2d32189107c1557c
LuffyKing <aderinwale17@gmail.com>
2018-05-19 10:23:25 +0100

    [Feature #157581106] Added modifyARequestChecker validator to the route

--- server/router/index.js
+++ server/router/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import RequestsController from '../controllers/Requests';
 import { getARequestChecker } from '../validation/getARequestValidator';
 import { createARequestChecker } from '../validation/createARequestValidator';
+import modifyARequestChecker from '../validation/modifyARequestChecker';
 
 const router = express.Router();
 
 router.get('/users/requests', RequestsController.getAllRequests);
 router.get('/users/requests/:requestid', getARequestChecker, RequestsController.getARequest);
 router.post('/users/requests/', createARequestChecker, RequestsController.createARequest);
-router.put('/users/requests/:requestid', getARequestChecker, RequestsController.updateARequest);
+router.put('/users/requests/:requestid', getARequestChecker, modifyARequestChecker, RequestsController.updateARequest);
 export default router;
