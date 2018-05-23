56e798350c1e476ea1d5e1500e0a89bd641be5d5
LuffyKing <aderinwale17@gmail.com>
2018-05-23 23:41:23 +0100

    [Feature #157581219] Updated to use generic handlers

--- server/validation/loginAUSerValidator.js
+++ server/validation/loginAUSerValidator.js
@@ -4,18 +4,18 @@ const loginAUserChecker = (req, res, next) => {
   const reqBody = getReqBody(req, ['email', 'password']);
   // check if the fields are filled
   let reply;
-  reply = emptyFieldsHandler(reqBody, res);
+  reply = emptyFieldsHandler(reqBody, res, 'Your log in attempt was unsuccessful because');
   if (reply) {
     return reply;
   }
   // check for strings
-  reply = nonStringFieldHandler(reqBody, res);
+  reply = nonStringFieldHandler(reqBody, res, 'Your log in attempt was unsuccessful because');
   if (reply) {
     return reply;
   }
 
   // check for valid types
-  reply = invalidFieldHandler(reqBody, res);
+  reply = invalidFieldHandler(reqBody, res, 'Your log in attempt was unsuccessful because');
   if (reply) {
     return reply;
   }
