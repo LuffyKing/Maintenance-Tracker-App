67df76ddad47fd6e1f970297d122a51311adf9e2
LuffyKing <aderinwale17@gmail.com>
2018-05-23 23:43:19 +0100

    [Feature #157581219] Tested validation and other login features

--- test/Users/login.js
+++ test/Users/login.js
@@ -6,6 +6,26 @@ const existingUser = {
   email: 'aderinwale17@gmail.com',
   password: 'test_password'
 };
+const existingUserWrongPassword = {
+  email: 'aderinwale17@gmail.com',
+  password: 'wrong_password'
+};
+const existingUserInvalidEmail = {
+  email: 'invalid17@gmail.com',
+  password: 'wrong_password'
+};
+const existingUserEmptyStrings = {
+  email: '',
+  password: ''
+};
+const existingUserEmptyObject = {
+
+};
+const existingUserNonStringInput = {
+  email: 1,
+  password: 1
+};
+
 chai.should();
 chai.use(chaiHttp);
 describe('Requests login Tests', () => {
@@ -25,6 +45,71 @@ describe('Requests login Tests', () => {
           res.body.user.department.should.eql('Guardians');
           res.body.user.profile.should.eql('User');
           res.body.user.location.should.eql('4 Tawdry Lane');
+          done();
+        });
+    });
+  });
+  describe('/POST users', () => {
+    it('should POST valid user email put invalid password and fail', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send(existingUserWrongPassword)
+        .end((err, res) => {
+          res.body.message.should.eql('Invalid Username/Password');
+          res.should.have.status(401);
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+  });
+  describe('/POST users', () => {
+    it('should POST invalid user email and fail', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send(existingUserInvalidEmail)
+        .end((err, res) => {
+          res.body.message.should.eql('Invalid Username/Password');
+          res.should.have.status(401);
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+  });
+  describe('/POST users', () => {
+    it('should POST empty strings for email and username, and fail', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send(existingUserEmptyStrings)
+        .end((err, res) => {
+          res.body.message.should.eql('Your log in attempt was unsuccessful because the email value is not an email ,the PASSWORD field did not contain a single letter of the alphabet');
+          res.should.have.status(400);
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+  });
+  describe('/POST users', () => {
+    it('should POST an empty object, and fail', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send(existingUserEmptyObject)
+        .end((err, res) => {
+          res.body.message.should.eql('Your log in attempt was unsuccessful because the fields EMAIL ,PASSWORD were not provided');
+          res.should.have.status(400);
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+  });
+  describe('/POST users', () => {
+    it('should POST an object wtith non string input and fail', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send(existingUserNonStringInput)
+        .end((err, res) => {
+          res.body.message.should.eql('Your log in attempt was unsuccessful because the fields EMAIL ,PASSWORD were supposed to be strings');
+          res.should.have.status(400);
+          res.body.should.have.property('message');
           done();
         });
     });
