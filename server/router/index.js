f93de6ad91e6aa6359f623593444749c07a3ded6
LuffyKing <aderinwale17@gmail.com>
2018-05-24 20:25:43 +0100

    [Feature #157733098] JWT authMiddleware incorporated in router

--- server/router/index.js
+++ server/router/index.js
@@ -6,12 +6,13 @@ import { getARequestChecker } from '../validation/getARequestValidator';
 import { createARequestChecker } from '../validation/createARequestValidator';
 import signUpAUserChecker from '../validation/signUpAUserValidator';
 import modifyARequestChecker from '../validation/modifyARequestChecker';
+import verifyToken from '../authMiddleware/jwt';
 
 const router = express.Router();
 
 router.post('/auth/signup', signUpAUserChecker, UsersController.signUp);
 router.post('/auth/login', loginAUserChecker, UsersController.login);
-router.get('/users/requests', RequestsController.getAllRequests);
+router.get('/users/requests', verifyToken, RequestsController.getAllRequests);
 router.get('/users/requests/:requestid', getARequestChecker, RequestsController.getARequest);
 router.post('/users/requests/', createARequestChecker, RequestsController.createARequest);
 router.put('/users/requests/:requestid', getARequestChecker, modifyARequestChecker, RequestsController.updateARequest);
