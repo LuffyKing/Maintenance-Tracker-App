82facc180df7a71b66c4a78e6139952596901231
LuffyKing <aderinwale17@gmail.com>
2018-05-24 22:19:45 +0100

    [Feature #157733096] To start last

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
-import './requests/getARequest';
 import './requests/createARequest';
-import './requests/modifyARequest';
 import './requests/badApiRequest';
 import './Users/login';
 import './Users/signup';
 import './requests/getAllRequests';
+import './requests/getARequest';
