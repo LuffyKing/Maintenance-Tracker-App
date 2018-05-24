bf1e21b55f2879ff16051d5d12ed3de6052a7e4c
LuffyKing <aderinwale17@gmail.com>
2018-05-24 20:21:55 +0100

    [Feature 157733098] Remove functions that were not being used and inserted requests

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 import { Pool } from 'pg';
 import configJs from '../config/config';
 import { loginValues } from './seeds/loginSeed';
+import { requestsColumns, requestsValues } from './seeds/requestsSeed';
 
 const env = process.env.NODE_ENV || 'development';
 const config = configJs[env];
@@ -8,48 +9,23 @@ const connectionString = config.DATABASE_URL || process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString
 });
-
-const createEnum = (enumName, values) => {
-  pool.query(`select exists (select 1 from pg_type where typname like '${enumName}');`)
-    .then(res => res.rows[0])
-    .catch(e => e.stack)
-    .then((exists) => {
-      if (!exists.exists) {
-        pool.query(`CREATE TYPE ${enumName} AS ENUM (${values
-          .map(item => `'${item}'`)
-          .join(', ')});`)
-          .catch(e => e.stack);
-      }
-    });
-};
-
-const seedAuto = (table, columns, values, poolClient) => {
-  poolClient.connect((err, client, done) => {
-    if (err) throw err;
-    client.query(`INSERT INTO ${table}(${columns}) VALUES(${values
-      .map(item => `'${item}'`)
-      .join(', ')});`, values, (err) => {
-      done();
-
-      if (err) {
-        return err.stack;
-      }
-    });
-  });
-};
-
-createEnum('status', ['Not Aprroved/Rejected', 'Approved', 'Rejected', 'Resolved']);
-
-createEnum('profile', ['Admin', 'User']);
-
-createEnum('reqtype', ['Maintenance', 'Repair']);
 pool.connect((err, client, done) => {
   if (err) throw err;
   if (process.env.NODE_ENV === 'test') {
     client.query(
-      'INSERT INTO USERS(ID,FIRST_NAME,LAST_NAME,EMAIL,PASSWORD,JOB_TITLE,DEPARTMENT,PROFILE,LOCATION,UPGRADE_ID) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);',
-      loginValues, () => {
+      'INSERT INTO USERS(ID,FIRST_NAME,LAST_NAME,EMAIL,PASSWORD,JOB_TITLE,DEPARTMENT,PROFILE,LOCATION,UPGRADE_ID) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *;',
+      loginValues, (error) => {
         done();
+        if (error) {
+          throw error;
+        }
+        client.query(
+          `INSERT INTO REQUESTS(${requestsColumns}) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9),($10,$11,$12,$13,$14,$15,$16,$17,$18);`,
+          requestsValues, (error1) => {
+            if (error1) throw error1;
+            done();
+          }
+        );
       }
     );
   }
