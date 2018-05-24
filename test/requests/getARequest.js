ea8e68bb8a423fb5479497d33ac4a338f6702815
LuffyKing <aderinwale17@gmail.com>
2018-05-24 22:19:10 +0100

    [Feature #157733096] Setup tests to work with databases

--- test/requests/getARequest.js
+++ test/requests/getARequest.js
@@ -1,8 +1,12 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../server/server';
-import { requests } from '../../server/dummy-data/database';
+import { requestsValues } from '../../server/db/seeds/requestsSeed';
 
+const existingUser = {
+  email: 'aderinwale17@gmail.com',
+  password: 'test_password'
+};
 chai.should();
 chai.use(chaiHttp);
 describe('Requests API Tests', () => {
@@ -10,41 +14,59 @@ describe('Requests API Tests', () => {
    * Testing get request on the Requests API -success case
    */
   describe('/GET requests', () => {
-    it('should GET try to get a request and succeed', (done) => {
+    it('should GET all requests with valid token', (done) => {
       chai.request(server)
-        .get('/api/v1/users/requests/0')
-        .end((err, response) => {
-          response.should.have.status(200);
-          response.body.should.have.property('message');
-          response.body.message.should.eql('Success - repair/maintenance request retrieved.');
-          response.body.request.should.eql(requests[0]);
-          done();
+        .post('/api/v1/auth/login')
+        .send(existingUser)
+        .end((err, responseLogin) => {
+          responseLogin.body.should.have.property('token');
+          chai.request(server)
+            .get(`/api/v1/users/requests/${requestsValues[0]}`)
+            .set('authorization', responseLogin.body.token)
+            .end((err, response) => {
+              response.body.message.should.eql('Your request has been found');
+              response.should.have.status(200);
+              response.body.request.title.should.eql('Broken Toilet');
+              done();
+            });
         });
     });
   });
 
   describe('/GET requests', () => {
-    it('should try to GET a request with an id that does not exist and fail', (done) => {
+    it('should GET all requests with valid token', (done) => {
       chai.request(server)
-        .get('/api/v1/users/requests/4')
-        .end((err, response) => {
-          response.should.have.status(404);
-          response.body.should.have.property('message');
-          response.body.message.should.eql('Maintenance/Repair with the specified id was not found');
-          done();
+        .post('/api/v1/auth/login')
+        .send(existingUser)
+        .end((err, responseLogin) => {
+          responseLogin.body.should.have.property('token');
+          chai.request(server)
+            .get('/api/v1/users/requests/1')
+            .set('authorization', responseLogin.body.token)
+            .end((err, response) => {
+              response.body.message.should.eql('The id provided is invalid because it is not of the type UUID 4');
+              response.should.have.status(400);
+              done();
+            });
         });
     });
   });
 
   describe('/GET requests', () => {
-    it('should try to GET a request with an invalid requestid and fail', (done) => {
+    it('should GET all requests with valid token', (done) => {
       chai.request(server)
-        .get('/api/v1/users/requests/badid')
-        .end((err, response) => {
-          response.should.have.status(400);
-          response.body.should.have.property('message');
-          response.body.message.should.eql('The id provided is invalid because it is not an integer');
-          done();
+        .post('/api/v1/auth/login')
+        .send(existingUser)
+        .end((err, responseLogin) => {
+          responseLogin.body.should.have.property('token');
+          chai.request(server)
+            .get('/api/v1/users/requests/[]')
+            .set('authorization', responseLogin.body.token)
+            .end((err, response) => {
+              response.body.message.should.eql('The id provided is invalid because it is not of the type UUID 4');
+              response.should.have.status(400);
+              done();
+            });
         });
     });
   });
